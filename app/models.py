b12a058b818a2d6d0bbc5f7d15d3b30aeda71cb4
Evance23 <evancebarracks01@gmail.com>
2021-05-09 22:10:41 +0300

    Add models

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Quote:
+    
+    def __init__(self,author,quote):
+        self.author=author
+        self.quote=quote
+class User (UserMixin, db.Model):
+    __tablename__ ='users'
+    id= db.Column(db.Integer,primary_key =True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('Inaccessible password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+    def __repr__(self):
+        return f'User {self.username}'
