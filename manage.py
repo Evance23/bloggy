190d284ae7674f8c778fb7799ea4ba75612c2e68
Evance23 <evancebarracks01@gmail.com>
2021-05-09 21:29:32 +0300

    Add the app routes

---
+++ manage.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    render_template ('about.html')
+
+@app.route('post')
+def post():
+    render_template('post.html')
+
+@app.route('/contact')
+def contact():
+    render_template('contact.html')
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__:'
+
+app.run(debug=True)
