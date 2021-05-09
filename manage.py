1a1edbeae1092f293d3bdf17fb3cf853d1956b3d
Evance23 <evancebarracks01@gmail.com>
2021-05-09 21:56:07 +0300

    Add changes to manage file

--- manage.py
+++ manage.py
@@ -1,34 +1,29 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Blog
+from flask_migrate import Migrate, MigrateCommand
 
 
-@app.route('/about')
-def about():
-    return render_template ('about.html')
+# Creating app instance
+app = create_app('production')
 
-@app.route('post')
-def post():
-    return render_template('post.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+manager = Manager(app)
+migrate = Migrate(app, db)
 
 
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-
-
-
-
-
-
-
-
+# @manager.shell
+# def make_shell_context():
+#     return dict(app=app, db=db, User=User)
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
